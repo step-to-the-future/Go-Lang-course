9ffa6c14b113669099e8d7bc531224fd131b63ba
Bro Aizek <ag9508542@gmail.com>
2025-10-10 14:00:02 +0500

    Create an array, find the smallest number!

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,7 @@
 package main
+
+import "github.com/step-to-the-future/Go-Lang-course/internal/logic"
+
+func main() {
+	logic.Cheest()
+}
--- internal/logic/logic.go
+++ internal/logic/logic.go
@@ -1,1 +1,27 @@
 package logic
+
+import "fmt"
+
+func Cheest() {
+	var n int
+	fmt.Print("Please enter max numbers of elements in an array: ")
+	fmt.Scanln(&n)
+	var arr []int
+	var r int
+	fmt.Println("Please enter elements of an array: ")
+	for i := 0; i < n; i++ {
+		fmt.Printf("Please enter the %v number: ", i+1)
+		fmt.Scanln(&r)
+		arr = append(arr, r)
+	}
+
+	fmt.Println("\nThe array is:", arr)
+
+	var minVal int = arr[0]
+	for y := 1; y < n; y++ {
+		if minVal > arr[y] {
+			minVal = arr[y]
+		}
+	}
+	fmt.Println("\nThe min val in array is", minVal)
+}
