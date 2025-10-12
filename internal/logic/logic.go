d32bc71626a2c0af5b62d6b5480578794da552c9
Bro Aizek <ag9508542@gmail.com>
2025-10-12 18:07:11 +0500

    Optimizedd version of phonebook search!

--- internal/logic/logic.go
+++ internal/logic/logic.go
@@ -6,13 +6,13 @@ func Findnumber(h map[string]string, names string) {
 	fmt.Print("Please enter the name: ")
 	fmt.Scanln(&names)
 	switch names {
-	case "Aziz":
+	case "Aziz", "aziz":
 		fmt.Printf("\nThe phone-number is %v \n", h["Aziz"])
-	case "Jasur":
+	case "Jasur", "jasur":
 		fmt.Printf("\nThe phone-number is %v \n", h["Jasur"])
-	case "Mukhammad":
+	case "Mukhammad", "mukhammad":
 		fmt.Printf("\nThe phone-number is %v \n", h["Mukhammad"])
-	case "Rinat":
+	case "Rinat", "rinat":
 		fmt.Printf("\nThe phone-number is %v \n", h["Rinat"])
 	default:
 		fmt.Println("\nName doesn't exist, try other name")
